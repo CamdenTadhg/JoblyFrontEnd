19035363792fcda0094b9519843d5a3f8534c6a3
camdentadhg <camdent@gmail.com>
2024-07-30 15:01:48 -0500

    job card created

--- jobly/src/JobCard.jsx
+++ jobly/src/JobCard.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ApplyButton from './ApplyButton';
+import {Card, CardTitle, CardBody, CardText} from 'reactstrap';
+import './JobCard.css';
 
-function JobCard(){
+function JobCard({title, salary, equity}){
     return(
-        <div>
-            I am a job card.
-            <ApplyButton/>
-        </div>
+        <Card className="JobCard-card">
+            <CardBody>
+                <CardTitle><b>{title}</b></CardTitle>
+                <CardText>
+                    <div>Salary: {salary}</div>
+                    <div>Equity: {equity}</div>
+                </CardText>
+                <ApplyButton/>
+            </CardBody>
+
+        </Card>
     )
 }
 
