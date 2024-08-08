fd7d51fbec2438e9bd135e0533a10a6d2825d695
camdentadhg <camdent@gmail.com>
2024-08-08 16:48:04 -0500

    JobList tests working

--- jobly/src/tests/JobList.test.jsx
+++ jobly/src/tests/JobList.test.jsx
@@ -1,4 +1,4 @@
-import {fireEvent, render, waitFor, cleanup} from '@testing-library/react';
+import {render, waitFor, cleanup} from '@testing-library/react';
 import {test, expect, vi, afterEach, beforeEach} from 'vitest';
 import {MemoryRouter} from 'react-router-dom';
 import JobList from '../JobList';
@@ -17,12 +17,27 @@ const Wrapper = ({children}) => {
     );
 };
 
+let spyGetAllJobs;
+
+vi.mock('react-router-dom', async (importOriginal) => {
+    const actual = await importOriginal();
+    return {
+        ...actual,
+        Navigate: vi.fn(({to}) => (`Redirected to ${to}`))
+    }
+});
+
 beforeEach(() => {
     const mock = new MockAdapter(axios);
     mock.onGet('http://localhost:3001/jobs').reply(200, {jobs: [
         {title: 'Accommodation manager', salary: 126000, equity: '0', companyHandle:'anderson-arias-morrow', applied: true},
         {title: 'Accountant, chartered certified', salary: 175000, equity: '0.1', companyHandle: 'arnold-berger-townsend', applied: false}
       ]});
+    mock.onGet('http://localhost:3001/jobs?').reply(200, {jobs: [
+        {title: 'Accommodation manager', salary: 126000, equity: '0', companyHandle:'anderson-arias-morrow', applied: true},
+        {title: 'Accountant, chartered certified', salary: 175000, equity: '0.1', companyHandle: 'arnold-berger-townsend', applied: false}
+      ]})
+    spyGetAllJobs = vi.spyOn(JoblyApi, 'getAllJobs');
 });
 
 afterEach(cleanup);
@@ -31,10 +46,31 @@ test('renders the JobList component', () => {
     render(<JobList/>, {wrapper: Wrapper});
 });
 
-test('matches the snapshot', () => {});
+test('matches the snapshot', async () => {
+    const jobList = render(<JobList/>, {wrapper: Wrapper});
+    await waitFor(() => {
+        expect(JoblyApi.getAllJobs).toHaveBeenCalled();
+    });
+    expect(jobList).toMatchSnapshot();
+});
 
-test('displays the correct content', () => {});
+test('displays the correct content', async () => {
+    const {getByText} = render(<JobList/>, {wrapper: Wrapper});
+    await waitFor(() => {
+        expect(JoblyApi.getAllJobs).toHaveBeenCalled();
+    })
+    expect(getByText('Accommodation manager')).toBeInTheDocument();
+});
 
-test('displays loading if loading', () => {});
+test('displays loading if loading', () => {
+    const {getByText} = render(<JobList/>, {wrapper: Wrapper});
+    expect(getByText('Loading', {exact: false})).toBeInTheDocument();
+});
 
-test('redirects to home if not logged in', () =>{});
+test('redirects to home if not logged in', () =>{
+    const {getByText} = render(
+        <MemoryRouter>
+            <JobList />
+        </MemoryRouter>);
+    expect(getByText('Redirected to /')).toBeInTheDocument();
+});
